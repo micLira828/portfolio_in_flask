cdeb1777ec0ef8f9eea1ecf6dd2bb02b61ee0864
micLira828 <micgep1015@gmail.com>
2026-01-04 22:49:25 -0800

    Added config

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = MAIL_USERNAME
